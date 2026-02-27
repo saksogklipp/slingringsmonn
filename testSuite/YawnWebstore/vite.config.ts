f3d7686a7d295a2562becf5aa53fcab386d023d2
Christer Nygaard <christer.nygaard@leaddesk.com>
2026-02-27 14:25:45 +0100

    change

--- testSuite/YawnWebstore/vite.config.ts
+++ testSuite/YawnWebstore/vite.config.ts
@@ -1,9 +1,5 @@
 ﻿import { defineConfig } from 'vite';
 
-// Basic Vite configuration for the Yawn Webstore app.
 export default defineConfig({
-  server: {
-    port: 5173,
-    strictPort: false,
-  },
+  base: '/testSuite/YawnWebstore/',
 });
